2078277378f07b4bd9026302e379e3bfa7c36c0b
ashkenda <ashkenda@msu.edu>
2026-05-21 21:12:26 +0000

    Deploying to gh-pages from @ ashkenda/ashkenda.github.io@3cbb552857dd61932bf73797cc84c5fa2ea89bba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-master-39-s-thesis",
+          title: 'master&amp;#39;s thesis',
+          description: "predicting algal toxin with machine learning",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
