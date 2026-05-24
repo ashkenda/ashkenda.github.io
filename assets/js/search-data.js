cd5c5d8fc75c5dca39b048ccc2f68a9bf8cbbebc
ashkenda <ashkenda@msu.edu>
2026-05-24 20:50:05 +0000

    Deploying to gh-pages from @ ashkenda/ashkenda.github.io@995604171e60ee7dfbe40975f4ec6dabb250b0c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -121,28 +121,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/chartjs/";
           
         },
-      },{id: "post-a-post-with-bibliography",
-        
-          title: "a post with bibliography",
-        
-        description: "an example of a blog post with bibliography",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/post-bibliography/";
-          
-        },
-      },{id: "post-a-post-with-custom-blockquotes",
-        
-          title: "a post with custom blockquotes",
-        
-        description: "an example of a blog post with custom blockquotes",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/custom-blockquotes/";
-          
-        },
       },{id: "post-a-post-with-videos",
         
           title: "a post with videos",
