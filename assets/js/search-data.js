88a77d35d806e9597f0d8cd56aed808cd382dd50
ashkenda <ashkenda@msu.edu>
2026-05-24 20:14:23 +0000

    Deploying to gh-pages from @ ashkenda/ashkenda.github.io@1e9ae1b4cb13890776ac598a37c6f169554ba373 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,19 +30,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-fun",
-          title: "fun!",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/fun!/index.html";
-          },
         },{id: "nav-outreach",
           title: "outreach",
           description: "science communication, public engagement, &amp; community science",
           section: "Navigation",
           handler: () => {
             window.location.href = "/outreach/";
+          },
+        },{id: "nav-fun",
+          title: "fun!",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/fun!/index.html";
           },
         },{id: "post-a-post-with-plotly-js",
         
