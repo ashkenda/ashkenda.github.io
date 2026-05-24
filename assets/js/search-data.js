454f6cc07f8641faa0070cb9b95ba097cea2155f
ashkenda <ashkenda@msu.edu>
2026-05-24 20:51:17 +0000

    Deploying to gh-pages from @ ashkenda/ashkenda.github.io@258e901b719720a3c3a1d16bac855edeec17e66a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -77,17 +77,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/advanced-images/";
           
         },
-      },{id: "post-a-post-with-echarts",
-        
-          title: "a post with echarts",
-        
-        description: "this is what included echarts code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/echarts/";
-          
-        },
       },{id: "post-a-post-with-chart-js",
         
           title: "a post with chart.js",
