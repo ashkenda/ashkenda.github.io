643dcb3c27f1ee0c0c692c5638f90d72975f25c6
ashkenda <ashkenda@msu.edu>
2026-05-24 20:42:49 +0000

    Deploying to gh-pages from @ ashkenda/ashkenda.github.io@e9f9e6a3eee3bce4d596d9e977888350bacdabb8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -88,17 +88,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/advanced-images/";
           
         },
-      },{id: "post-a-post-with-vega-lite",
-        
-          title: "a post with vega lite",
-        
-        description: "this is what included vega lite code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/vega-lite/";
-          
-        },
       },{id: "post-a-post-with-geojson",
         
           title: "a post with geojson",
@@ -130,17 +119,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2024/chartjs/";
-          
-        },
-      },{id: "post-a-post-with-tikzjax",
-        
-          title: "a post with TikZJax",
-        
-        description: "this is what included TikZ code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tikzjax/";
           
         },
       },{id: "post-a-post-with-bibliography",
