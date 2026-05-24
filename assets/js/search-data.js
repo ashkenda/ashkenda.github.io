8fc59bce7e801fdb70d611fde1130162106a106c
ashkenda <ashkenda@msu.edu>
2026-05-24 20:43:07 +0000

    Deploying to gh-pages from @ ashkenda/ashkenda.github.io@1a8f9dbc86c76ee660adb8a5f50731b8e4249213 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -264,6 +264,17 @@ ninja.data = [{
             window.location.href = "/blog/2020/twitter/";
           
         },
+      },{id: "post-a-post-with-math",
+        
+          title: "a post with math",
+        
+        description: "an example of a blog post with some math",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/math/";
+          
+        },
       },{id: "post-a-post-with-images",
         
           title: "a post with images",
